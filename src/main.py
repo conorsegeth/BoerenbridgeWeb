8d38b7b65cb39f4ebb48ff9d9de7fb5d9916afb0
conorsegeth <segethconor@gmail.com>
2023-01-24 18:50:31 +0100

    Inital

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def render():
+    return render_template("index.html")
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
