6e4895434a3929f38571605b8343cf270a966e1d
conorsegeth <segethconor@gmail.com>
2023-01-24 22:17:15 +0100

    added sidebar to index site

--- src/static/scripts/main.js
+++ src/static/scripts/main.js
@@ -1,2 +1,7 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello World!";
+function openNav() {
+    document.getElementById("infoSidebar").style.width = "600px";
+  }
+
+  function closeNav() {
+    document.getElementById("infoSidebar").style.width = "0";
+  }
