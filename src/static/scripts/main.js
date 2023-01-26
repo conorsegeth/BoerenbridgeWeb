68b73896fc4715be94f4ad363f598e276e5ed3c2
conorsegeth <segethconor@gmail.com>
2023-01-26 14:36:10 +0100

    website progress

--- src/static/scripts/main.js
+++ src/static/scripts/main.js
@@ -5,3 +5,11 @@ function openNav() {
   function closeNav() {
     document.getElementById("infoSidebar").style.width = "0";
   }
+
+  function openWin() {
+    document.getElementById("settingsModal").style.display = "block";
+  }
+
+  function closeWin() {
+    document.getElementById("settingsModal").style.display = "none";
+  }
