5cff9437e1744a4d611187d4c7f58920cb1b7a46
conorsegeth <segethconor@gmail.com>
2023-02-10 12:20:19 +0100

    working on rooms

--- src/static/scripts/main.js
+++ src/static/scripts/main.js
@@ -1,15 +1,29 @@
 function openNav() {
-  document.getElementById("infoSidebar").style.width = "600px";
+    document.getElementById("infoSidebar").style.width = "600px";
 }
 
 function closeNav() {
-  document.getElementById("infoSidebar").style.width = "0";
+    document.getElementById("infoSidebar").style.width = "0";
 }
 
 function openWin() {
-  document.getElementById("settingsModal").style.display = "block";
+    document.getElementById("settingsModal").style.display = "block";
 }
 
 function closeWin() {
-  document.getElementById("settingsModal").style.display = "none";
+    document.getElementById("settingsModal").style.display = "none";
 }
+
+// Socket-IO
+var socket = io()
+
+var room_dict = {};
+
+// Fix whatever going on here
+socket.on("update room", function(data) {
+    var room_id = data.room_id;
+    var players = data.players;
+    var state = data.state;
+
+    room_dict[room_id] = {"players": players, "state": state}
+});
